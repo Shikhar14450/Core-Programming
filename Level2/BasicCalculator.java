2b52a1a83892b580e91d12054887c49f71cec998
shikhar <shikhargusain5@gmail.com>
2025-07-24 12:02:21 +0530

    24-07-2025

---
+++ Level2/BasicCalculator.java
@@ -0,0 +1,17 @@
+package Level2;
+import java.util.*;
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number1: ");
+        double number1 = sc.nextDouble();
+        System.out.print("Enter number2: ");
+        double number2 = sc.nextDouble();
+        double add = number1 + number2;
+        double sub = number1 - number2;
+        double mul = number1 * number2;
+        double div = number1 / number2;
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is " + add + ", " + sub + ", " + mul + ", and " + div);
+    }
+}
